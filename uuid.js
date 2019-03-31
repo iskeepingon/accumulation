509a787fc0c9e85061085b3805b7ca926831900b
iskeeping <18279959396@163.com>
2019-03-31 18:37:09 +0800

    accumulation

--- uuid.js
+++ uuid.js
@@ -4,7 +4,7 @@
 
 var uuid = function () {
   var s = []
-  var hexDigits = "0123456789abcdef'
+  var hexDigits = '0123456789abcdef'
   for (var i = 0; i < 36; i++) {
     s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
   }
