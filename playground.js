8d7008651e14f1b2dca0432f2e29a0f56ff9b609
iskeeping <18279959396@163.com>
2019-04-02 21:34:26 +0800

    accumulation

--- playground.js
+++ playground.js
@@ -2,19 +2,22 @@ class Playground {
   /**
    * @param w 操场的宽度
    * @param h 操场的长度
-   * @param d
-   * 该对象待完善
+   * @param d 真实距离 比如跑道一圈400
+   * @param a,b 圆心
+   * @return {x:x,y:y}根据距离返回跑道上某一点的坐标
    */
-  constructor(w, h, d) {
+  constructor(w, h, d, a, b) {
     this.w = w
     this.h = h
     this.d = d
+    this.x = a || 0
+    this.y = b || 0
   }
 
   /**
-   * @param distance
-   * @returns {{x: *, y: *}}
-     */
+   * @param distance 跑了多少距离
+   * @returns {{x: *, y: *}} 坐标
+   */
   getCoordinate(distance) {
     let x, y
     let w = this.w
@@ -24,22 +27,23 @@ class Playground {
     let r = w / 2
     let a = 2 * h + 2 * PI * r//操场距离
     let unit = d / a
-    let c = Math.floor(distance / unit / a)
-    let l
+    let c = distance / unit
+    let t = 2 * PI / 360
+    let l//一圈半,两圈半,坐标点在同一个位置
 
-    if (c > 0) {
-      l = distance / unit - c * a
+    if (c > a) {
+      l = c % a
     } else {
-      l = distance / unit
+      l = c
     }
 
     if (l >= 0 && l <= h / 2) {
       x = r
       y = l
     } else if (l > h / 2 && l < (h / 2 + PI * r)) {
-      let degree = 360 * (l - h / 2) / (2 * PI * r)
-      x = r * Math.cos(degree)
-      y = h / 2 + r * Math.sin(degree)
+      let hudu = ((l - h / 2) / r) * t
+      x = r * Math.cos(hudu)
+      y = h / 2 + r * Math.sin(hudu)
     } else if (l >= (h / 2 + PI * r) && l <= ( h + PI * r)) {
       x = -r
       y = l - (h / 2 + PI * r)
@@ -47,25 +51,27 @@ class Playground {
       x = -r
       y = -(l - ( h + PI * r))
     } else if (l > (3 * h / 2 + PI * r) && l < 2 * h + 2 * PI * r) {
-      let degree = 360 * (l - 3 * h / 2 - PI * r) / (2 * PI * r)
-      x = -r * Math.cos(degree)
-      y = -h / 2 - r * Math.sin(degree)
+      let hudu = ((l - 3 * h / 2 - PI * r) / r ) * t
+      x = -r * Math.cos(hudu)
+      y = -h / 2 - r * Math.sin(hudu)
     } else {
       x = r
       y = l - (2 * h + 2 * PI * r)
     }
 
     return {
-      x: x,
-      y: y
+      x: x + a,
+      y: y + b
     }
   }
 }
 
 
 let p = new Playground(750, 300, 750 * Math.PI + 600)
-let json = p.getCoordinate((750 * Math.PI + 600) / 2)
-console.log(json)
+// let json = p.getCoordinate(750 * Math.PI / 4 + 150)
+let json1 = p.getCoordinate(150 + (Math.PI / 4) * 375)
+// console.log(json)
+console.log(json1)
 /*export {
  Playground
  }*/
