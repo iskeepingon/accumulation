d0d3376eeea481d839e88f6c346dad16f8e41845
iskeeping <18279959396@163.com>
2019-04-03 18:21:24 +0800

    accumulation

--- sorts.js
+++ sorts.js
@@ -56,9 +56,9 @@ var quickSort = function (arr) {
   }
   return quickSort(left).concat([pivot], quickSort(right))
 }
-var quickSortArr = [1, 2, 9, 0, 3, 6]
-var quickSortArr1 = quickSort(quickSortArr)
-console.log(quickSortArr1)
+// var quickSortArr = [1, 2, 9, 0, 3, 6]
+// var quickSortArr1 = quickSort(quickSortArr)
+// console.log(quickSortArr1)
 
 /**
  * 插入排序
@@ -77,6 +77,98 @@ var insertSort = function (arr) {
   }
   return arrTemp
 }
-var insertSortArr = [1, 2, 9, 0, 3, 6]
-var insertSortArr1 = insertSort(insertSortArr)
-console.log(insertSortArr1)
+// var insertSortArr = [1, 2, 9, 0, 3, 6]
+// var insertSortArr1 = insertSort(insertSortArr)
+// console.log(insertSortArr1)
+
+/**
+ * 合并两个已经排好序的数组
+ * 归并排序
+ */
+var mergeSort = function (arr1, arr2) {
+  var result = []
+  while (arr1.length && arr2.length) {
+    if (arr1[0] > arr2[0]) {
+      result.push(arr2.shift())
+    } else {
+      result.push(arr1.shift())
+    }
+  }
+  return result.concat(arr1, arr2)
+}
+
+// var mergeSortData1 = [1, 2, 3, 4, 5, 9]
+// var mergeSortData2 = [5, 6, 7, 9, 10]
+// var mergeData3 = mergeSort(mergeSortData1, mergeSortData2)
+// console.log(mergeData3)
+
+/**
+ * @function 大数据量排序 先分组排序,然后再依次去取元素,重新生成新的排好序的数组
+ * @param arr
+ */
+var bigDataSort = function (arr, gap) {
+  //取最小数
+  var getMin = function (arrs) {
+    var min
+    var index
+    var temp
+    var temp1
+    for (var i = 0; i < arrs.length; i++) {
+      if (i === 0) {
+        min = arrs[0][0]
+        index = 0
+      } else {
+        temp = arrs[i][0]
+        if (temp < min) {
+          min = temp
+          index = i
+        }
+      }
+    }
+    temp1 = arrs[index]
+    temp1.shift()
+    if (temp1.length === 0) {
+      arrs.splice(index, 1)
+    }
+    return min
+  }
+  //分组并且对每一组都进行快速排序
+  var groupSort = function (arr, gap) {
+    var len = gap || 100
+    var arrs = []
+    var arrsLen = Math.ceil(arr.length / len)
+    for (var i = 0; i < arrsLen; i++) {
+      if (i === arrsLen - 1) {
+        arrs.push(quickSort(arr.slice(i * len, arrsLen * len)))
+      } else {
+        arrs.push(quickSort(arr.slice(i * len, (i + 1) * len)))
+      }
+    }
+    return arrs
+  }
+  var result = []
+  var arrs = groupSort(arr, gap)
+  while (arrs.length > 0) {
+    var min = getMin(arrs)
+    result.push(min)
+  }
+  return result
+}
+
+//创建大数据
+var createBigData = function (len) {
+  var bigData = []
+  for (var i = 0; i < len; i++) {
+    bigData.push(Math.floor(Math.random() * len))
+  }
+  return bigData
+}
+
+//获取当前时间 单位是ms
+var getCurrentTime = function () {
+  return (new Date()).getTime()
+}
+var start = getCurrentTime()
+var bigDataSortData = bigDataSort(createBigData(10000000), 50000)//对一千万个数字排序
+end = getCurrentTime()
+console.log('end-start=', (end - start) / 1000, 's')
