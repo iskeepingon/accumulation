2bfa598a3403cdde436ba51539efb175d6795cb8
iskeeping <18279959396@163.com>
2019-04-03 11:32:20 +0800

    accumulation

--- uuid.js
+++ uuid.js
@@ -3,10 +3,12 @@
  */
 
 var uuid = function () {
+  //重现一样的概率是Math.pow(1/16,36)
   var s = []
   var hexDigits = '0123456789abcdef'
   for (var i = 0; i < 36; i++) {
     s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+    // s[i] = hexDigits.substr(Math.floor(Math.random() * 16), 1)
   }
   s[14] = '4' // bits 12-15 of the time_hi_and_version field to 0010
   s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
