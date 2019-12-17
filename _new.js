e43b3ced077de215f48e073557f7c292e6b5584c
18279959396@163.com <18279959396@163.com>
2019-12-17 21:09:29 +0800

    init

--- _new.js
+++ _new.js
@@ -23,3 +23,48 @@ let a1 = new A('原生的new')
 
 console.log(a.getName())
 console.log(a1.getName())
+
+class Single {
+  constructor(name) {
+    if (typeof Single.instance === 'object') {
+      return Single.instance
+    }
+    Single.instance = this
+    this.name = name
+    return this
+  }
+}
+
+let s = new Single('a')
+let sb = new Single('b')
+console.log(s === sb)
+
+// async await的使用：
+
+let o = {
+  request() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let num = Math.floor(Math.random() * 10)
+        if (num > 5) {
+          resolve({code: 0, data: num})
+        } else {
+          reject({code: -1})
+        }
+      }, 1000)
+    })
+  },
+  async getData() {
+    let res = await this.request()
+    if (res.code == 0) {
+
+    }
+  },
+  init(){
+    this.getData().then(()=>{
+      console.log('ok')
+    }).catch(()=>{
+      console.log('fail')
+    })
+  }
+}
