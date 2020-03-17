299f9f925c33cd141d8d874dcc3821f7bb0c0c93
frontend <frontend@example.com>
2020-03-17 11:53:32 +0800

    init

--- isJSON.js
+++ isJSON.js
@@ -1,33 +1,4 @@
 /**
- * @function 递归
- * @param item {any}
- * @return true or false {boolean}
- */
-function isJsonRecursion(item) {
-  if (typeof item === 'object') {
-    const lowercase = Object.prototype.toString.call(item).toLowerCase()
-    if (lowercase === '[object array]') {
-      if (item.length === 0) {
-        return false
-      }
-      for (let i = 0; i < item.length; i++) {
-        let flag = isJsonRecursion(item[i])
-        if (!flag) {
-          return false
-        }
-      }
-      return true
-    } else if (lowercase === '[object object]') {
-      return true
-    } else {
-      return false
-    }
-  } else {
-    return false
-  }
-}
-
-/**
  * @function 判断obj是否为json对象
  * @param obj {any}
  * @returns {boolean}
@@ -37,7 +8,7 @@ function isJSON(obj) {
     const lowercase = Object.prototype.toString.call(obj).toLowerCase()
     if (lowercase === '[object array]') {
       for (let i = 0; i < obj.length; i++) {
-        let flag = isJsonRecursion(obj[i])
+        let flag = isJSON(obj[i])
         if (!flag) {
           return false
         }
