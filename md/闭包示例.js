bb4dc7aa19b759c99e391f859a2b590707d04e2b
frontend <frontend@example.com>
2020-03-26 14:08:45 +0800

    init

--- md/闭包示例.js
+++ md/闭包示例.js
@@ -31,3 +31,29 @@ for (var i = 0; i < liDomList.length; i++) {
     }
   })(i)
 }
+
+//使用
+
+//闭包实现只读，只写，可读可写
+
+var property = (function (opts) {
+  var property = '闭包'
+  var hasSet = opts.hasSet || false
+  var hasGet = opts.hasGet || false
+  var res = {}
+  if (hasGet) {
+    res.get = function () {
+      return property
+    }
+  }
+  if (hasSet) {
+    res.set = function (v) {
+      property = v
+    }
+  }
+  return res
+})({hasSet: true, hasGet: true})
+
+console.log(property.get())
+property.set('闭包DEMO')
+console.log(property.get())
