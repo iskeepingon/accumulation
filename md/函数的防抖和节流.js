da832bf7f57eb767f5fcff8907675fda1ded1dbc
iskeeping <18279959396@163.com>
2020-05-25 22:45:22 +0800

    init

---
+++ md/函数的防抖和节流.js
@@ -0,0 +1,78 @@
+/**
+ * @desc 函数防抖
+ * @param func 函数
+ * @param wait 延迟执行毫秒数
+ * @param immediate true 表立即执行，false 表非立即执行
+ */
+function debounce(func, wait, immediate) {
+  let timeout;
+
+  return function () {
+    let context = this;
+    let args = arguments;
+
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      var callNow = !timeout;
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, wait)
+      if (callNow) func.apply(context, args)
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args)
+      }, wait);
+    }
+  }
+}
+
+/**
+ * @desc 函数节流
+ * @param func 函数
+ * @param wait 延迟执行毫秒数
+ * @param type 1 表时间戳版，2 表定时器版
+ */
+function throttle(func, wait, type) {
+  let previous;
+  let timeout;
+  if (type === 1) {
+    previous = 0;
+  } else if (type === 2) {
+    timeout;
+  }
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (type === 1) {
+      let now = Date.now();
+
+      if (now - previous > wait) {
+        func.apply(context, args);
+        previous = now;
+      }
+    } else if (type === 2) {
+      if (!timeout) {
+        timeout = setTimeout(() => {
+          timeout = null;
+          func.apply(context, args)
+        }, wait)
+      }
+    }
+  }
+}
+
+let debounceCallback = debounce(function () {
+  console.log('debounceCallback')
+}, 1000, false)
+
+for (let i = 0; i < 10000000; i++) {
+  debounceCallback()
+}
+
+let throttleCallback = throttle(function () {
+  console.log('throttleCallback')
+}, 1000, false)
+
+for (let i = 0; i < 10000000; i++) {
+  throttleCallback()
+}
