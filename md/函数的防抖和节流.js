6442e2731102c17ab46f498fecd737538c7cb1fc
iskeeping <18279959396@163.com>
2020-05-26 00:03:46 +0800

    init

--- md/函数的防抖和节流.js
+++ md/函数的防抖和节流.js
@@ -5,23 +5,23 @@
  * @param immediate true 表立即执行，false 表非立即执行
  */
 function debounce(func, wait, immediate) {
-  let timeout;
+  let timeout
 
   return function () {
-    let context = this;
-    let args = arguments;
+    let context = this
+    let args = arguments
 
-    if (timeout) clearTimeout(timeout);
+    if (timeout) clearTimeout(timeout)
     if (immediate) {
-      let callNow = !timeout;
+      let callNow = !timeout
       timeout = setTimeout(() => {
-        timeout = null;
+        timeout = null
       }, wait)
       if (callNow) func.apply(context, args)
     } else {
       timeout = setTimeout(function () {
         func.apply(context, args)
-      }, wait);
+      }, wait)
     }
   }
 }
@@ -33,27 +33,27 @@ function debounce(func, wait, immediate) {
  * @param type 1 表时间戳版，2 表定时器版
  */
 function throttle(func, wait, type) {
-  let previous;
-  let timeout;
+  let previous
+  let timeout
   if (type === 1) {
-    previous = 0;
+    previous = 0
   } else if (type === 2) {
-    timeout;
+    timeout
   }
   return function () {
-    let context = this;
-    let args = arguments;
+    let context = this
+    let args = arguments
     if (type === 1) {
-      let now = Date.now();
+      let now = Date.now()
 
       if (now - previous > wait) {
-        func.apply(context, args);
-        previous = now;
+        func.apply(context, args)
+        previous = now
       }
     } else if (type === 2) {
       if (!timeout) {
         timeout = setTimeout(() => {
-          timeout = null;
+          timeout = null
           func.apply(context, args)
         }, wait)
       }
