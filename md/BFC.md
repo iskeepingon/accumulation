e5b54dbceb20f177dc5a5df0a63f669148681d10
iskeeping <18279959396@163.com>
2020-06-24 17:10:29 +0800

    init

--- md/BFC.md
+++ md/BFC.md
@@ -4,7 +4,7 @@
 
 ## BFC
 
-BFC全称Block Formatting Context，翻译块格式化上下文，BFC是Web页面的可视CSS渲染的一部分，是块盒子的布局过程发生的区域，也是浮动元素与其他元素交互的区域。
+块格式化上下文（Block Formatting Context，BFC）是Web页面的可视CSS渲染的一部分，是块盒子的布局过程发生的区域，也是浮动元素与其他元素交互的区域。
 
 ## 如何创建BFC
 
@@ -23,14 +23,9 @@ BFC全称Block Formatting Context，翻译块格式化上下文，BFC是Web页�
 + 多列容器（元素的column-count或column-width不为auto，包括column-count为 1）
 + column-span为all的元素始终会创建一个新的BFC，即使该元素没有包裹在一个多列容器中（标准变更，Chrome bug）。
 
-## BFC的描述
-
-块格式化上下文包含创建它的元素内部的所有内容.
-
-块格式化上下文对浮动定位（参见float）与清除浮动（参见 clear）都很重要。浮动定位和清除浮动时只会应用于同一个BFC内的元素。浮动不会影响其它BFC中元素的布局，而清除浮动只能清除同一BFC中在它前面的元素的浮动。外边距折叠（Margin collapsing）也只会发生在属于同一BFC的块级元素之间。
-
 ## BFC使用场景
 
-
++ 清除浮动
++ 
 
 谢谢阅读！
--- md/去重.js
+++ md/去重.js
@@ -1,24 +1,31 @@
-//方法一
+// //方法一
+// function unique(arr) {
+//   return [...new Set(arr)]
+// }
+// //方法二
+// function unique(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr.indexOf(arr[i]) != i) {
+//       arr.splice(i, 1)
+//       i--
+//     }
+//   }
+//   return arr
+// }
+// //方法三
+// function unique(arr) {
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     if (arr.indexOf(arr[i]) != i) {
+//       arr.splice(i, 1)
+//     }
+//   }
+//   return arr
+// }
+//方法四
 function unique(arr) {
-  return [...new Set(arr)]
-}
-//方法二
-function unique(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.indexOf(arr[i]) != i) {
-      arr.splice(i, 1)
-      i--
-    }
-  }
-  return arr
-}
-//方法三
-function unique(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    if (arr.indexOf(arr[i]) != i) {
-      arr.splice(i, 1)
-    }
-  }
+  arr.filter((item, index) => {
+    return arr.indexOf(item) == index
+  })
   return arr
 }
 
