66351f85121bef8c813dc5b2fe90f6134081b348
iskeeping <18279959396@163.com>
2020-06-24 17:24:03 +0800

    init

--- md/去重.js
+++ md/去重.js
@@ -1,33 +1,33 @@
-// //方法一
-// function unique(arr) {
-//   return [...new Set(arr)]
-// }
-// //方法二
-// function unique(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr.indexOf(arr[i]) != i) {
-//       arr.splice(i, 1)
-//       i--
-//     }
-//   }
-//   return arr
-// }
-// //方法三
-// function unique(arr) {
-//   for (let i = arr.length - 1; i > 0; i--) {
-//     if (arr.indexOf(arr[i]) != i) {
-//       arr.splice(i, 1)
-//     }
-//   }
-//   return arr
-// }
-//方法四
+//方法一
 function unique(arr) {
-  arr.filter((item, index) => {
-    return arr.indexOf(item) == index
-  })
+  return [...new Set(arr)]
+}
+//方法二
+function unique(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i]) != i) {
+      arr.splice(i, 1)
+      i--
+    }
+  }
   return arr
 }
+//方法三
+function unique(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr.indexOf(arr[i]) != i) {
+      arr.splice(i, 1)
+    }
+  }
+  return arr
+}
+//方法四
+function unique(arr) {
+  return arr.filter((item, index) => {
+    return arr.indexOf(item) == index
+  })
+}
+
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1]
 console.log(unique(arr))
